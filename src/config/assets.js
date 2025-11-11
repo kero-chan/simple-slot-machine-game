9b8e5665eea033e853049f970a70484a5f9dbb6d
Kevin Tsai <hokichaio@gmail.com>
2025-11-11 11:32:42 +0900

    remove unused assets

--- src/config/assets.js
+++ src/config/assets.js
@@ -3,8 +3,6 @@ export const ASSETS = {
     start_background: new URL('../assets/start_game_bg.jpg', import.meta.url).href,
     // Spin button assets: frame background + gold arrow
     spin: new URL('../assets/slotMachine/spin_btn/spinBtn.png', import.meta.url).href,
-    spin_frame: new URL('../assets/slotMachine/spin_btn/frame.png', import.meta.url).href,
-    spin_arrow: new URL('../assets/slotMachine/spin_btn/arrow.png', import.meta.url).href,
     // Tiles spritesheets
     tiles_50: new URL('../assets/slotMachine/tiles/50.png', import.meta.url).href,
     tiles_29: new URL('../assets/slotMachine/tiles/29.png', import.meta.url).href,
